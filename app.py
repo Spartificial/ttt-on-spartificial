0eca619af0d396df31a8427934afdbee99ac4b72
Yash <yash92328@gmail.com>
2021-11-22 21:30:47 +0530

    updates

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import torch
 import torch.optim as optim
 from flask import Flask, jsonify, request
 import json
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app)
 
 batch_size = 128
 gamma = 0.99
